5710efe2f848743a888c7c64fa89467a65b6c0be
amar-eng <am24mo@gmail.com>
2023-10-01 20:30:59 -0400

    Adding changes

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', async (req, res, next) => {
-  return res.status(200).json({
-    title: 'Express Testing',
-    message: 'The app is working properly!',
+  // Adjust the path to your data.json file given its location
+  const dataPath = path.join(__dirname, '..', 'data', 'data.json');
+
+  // Read the file asynchronously
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read data.' });
+    }
+
+    // Parse the data and send as a response
+    const jsonData = JSON.parse(data);
+    res.status(200).json(jsonData);
   });
 });
 
