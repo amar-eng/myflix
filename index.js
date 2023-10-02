2532a6ba7505eeff5fd225b0b7b3bfaf418e77c2
amar-eng <am24mo@gmail.com>
2023-10-01 21:17:12 -0400

    adding images

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 const movies = require('./routes/movies');
 const users = require('./routes/users');
 
 const app = express();
+
+// Middleware to parse JSON
 app.use(express.json());
 
+// Serve static assets from the "data/assets" directory
+app.use('/assets', express.static(path.join(__dirname, 'data', 'assets')));
+
+// Routes
 app.use('/movies', movies);
 app.use('/users', users);
 
